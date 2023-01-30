57422a19baf8755f25f15c9d5fd7942bd788be20
Atef Jlassi <Atef.jlassi@value.com.tn>
2023-01-30 09:30:41 +0100

    add comment

--- src/main/java/com/config/ProjectConfig.java
+++ src/main/java/com/config/ProjectConfig.java
@@ -30,7 +30,10 @@ public class ProjectConfig {
     /**
      * Second way of wiring Beans
      *
-     * By defining a cat parameter  'public Owner owner(Cat cat) {}' and not calling the method any more
+     * By defining a cat parameter  'public Owner owner(Cat cat) {}' and not calling the method any more.
+     * When you put a parameter in a method annotated @Bean, Spring will search in the context if there is
+     * a type of this instance already exist on the context.
+     *
      * owner.setCat(cat);
      */
     owner.setCat(cat);
--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
       Owner o = c.getBean(Owner.class);
 
       x.setName("Leo");
+
       System.out.println(x);
       System.out.println(o);
 
